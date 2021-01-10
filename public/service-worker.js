375de0c456e5b31884ce84c493337afd77214c35
mini pony <northjw1@gmail.com>
2021-01-10 13:21:39 -0500

    adding service-workers.js and files to cache

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+
+const FILES_TO_CACHE = [
+    "/",
+    "/db.js",
+    "/index.js",
+    "/manifest.json",
+    "/styles.css",
+    "/service-worker.js",
+    
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
